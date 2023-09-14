f1966591d0f9dfd563ce89a99f7e3db4fb41cf5b
elmiraaemi <108354246+elmiraaemi@users.noreply.github.com>
2023-09-14 23:02:46 +0330

    gif.py

--- gif.py
+++ gif.py
@@ -1,8 +1,9 @@
 import os
-import imageio.v2
-myfiles = os.listdir('image')
-images = []
-for i in range(len(myfiles)):
-    image = imageio.imread(myfiles[i])
-    images.append(image)
-imageio.mimsave('gif.gif', images)
+import imageio
+file_list=sorted(os.listdir("python/T8/image"))
+images=[]
+for file in file_list:
+    file_path="python/T8/imag/"+file
+    IMage=imageio.imread(file_path)
+    images.append(IMage)
+imageio.mimsave("python/T8/image/mygif.gif",images)
